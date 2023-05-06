db4aedd29881524dc5d958c2cdd302b295e8f2c4
yuhyojeong <128355344+yuhyojeong@users.noreply.github.com>
2023-05-06 22:21:51 +0900

    Commit #5
    added function findidx, created map instead of vector

--- board.h
+++ board.h
@@ -1,33 +1,39 @@
 #include <fstream>
-#include <list>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 #include "page.h"
 
 using std::endl;
 using std::ofstream;
-using std::pair;
+using std::vector;
+using std::map;
+using std::find;
 
 class Board {
     public:
         Board(int num_jobs, int width, int height, ofstream& output_stream);
         ~Board();
-
         void print_board();
         void print_job(int job_idx, char job_type, int id);
-        void set_board(); // boardlst의 각 list의 마지막 원소를 board로 set
-
         //job functions
-        void insert_page(int x, int y, int width, int height, int id, char content);
+        void insert_page(int x, int y, int pagewidth, int pageheight, int id, char content);
         void delete_page(int id);
         void modify_content(int id, char content);
         void modify_position(int id, int x, int y);
+        void set_board(); // boardlst의 각 list의 마지막 원소를 board로 set
+        void board_insert(int x, int y, int pagewidth, int pageheight, char content); //insert content to boardlst
+        void board_delete(int x, int y, int pagewidth, int pageheight); //delete content from boardlst
+        void deleteseq(int id); //page를 순차적으로 삭제
+        int findidx(int id); //해당 page 위의 page 중에 가장 index가 작은 것 return
 
     private:
         int num_jobs, width, height;
         ofstream& output; 
         char* board;
-        list <pair<int, Page>> pages; //page 넣은 순서대로 list에 저장
-        list<char>* boardlst; //겹치는 content를 list로 board에 넣음
+        map <int, Page> pagemap; //page의 id와 page를 저장
+        vector<char>* boardlst; //겹치는 content를 boardlst에 넣음
 };
 
 
@@ -37,7 +43,7 @@ Board::Board(int num_jobs, int width, int height, ofstream& output_stream): outp
     this->num_jobs = num_jobs;
 
     board = new char[width*height];
-    boardlst = new list<char>[width * height];
+    boardlst = new vector<char>[width * height];
 
     for (int h = 0; h < height; h++) {
         for (int w = 0; w < width; w++) {
@@ -92,24 +98,17 @@ void Board::print_job(int job_idx, char job_type, int id) {
 }
 
 
-void Board::insert_page(int x, int y, int width, int height, int id, char content) {
-    Page newpage = Page(x, y, width, height, content); // 해당 page 저장
-    pages.push_back(pair<int, Page> (id, newpage)); //list의 뒤에 id, page pair 삽입
-    for (int h = y; h < y + height; y++){
-        for (int w = x; w < x + width; w++){
-            boardlst[h * width + w].push_back(content);
-        }
-    }
+void Board::insert_page(int x, int y, int pagewidth, int pageheight, int id, char content) {
+    Page newpage = Page(x, y, pagewidth, pageheight, id, content); // 해당 page 저장
+    pagemap.insert({id, newpage}); // map에 id, page pair 삽입
+    board_insert(x, y, pagewidth, pageheight, content);
     set_board();
     print_board();
 }
 
 void Board::delete_page(int id) {
-    for (auto itr = pages.end() - 1; itr >= pages.begin(); itr--){
-        if ((*itr).first == id){
-            pages.erase(itr);
-        }
-    }
+    vector<int> prior;
+
 }
 
 void Board::modify_content(int id, char content) {
@@ -127,4 +126,45 @@ void Board::set_board(){
             board[h*width + w] = boardlst[h*width + w].back();
         }
     }
+}
+
+void Board::board_insert(int x, int y, int pagewidth, int pageheight, char content){
+    for (int h = y; h < y + pageheight; h++){
+        for (int w = x; w < x + pagewidth; w++){
+            boardlst[h * width + w].push_back(content); //boardlst의 끝에 content 삽입
+        }
+    }
+}
+
+void Board::board_delete(int x, int y, int pagewidth, int pageheight){
+    for (int h = y; h < y + pageheight; h++){
+        for (int w = x; w < x + pagewidth; w++){
+            boardlst[h * width + w].pop_back(); //boardlst의 마지막 원소 삭제
+        }
+    }
+}
+
+void Board::deleteseq(int id){
+    
+}
+
+int Board::findidx(int id){
+    int x = pagemap[id].getx();
+    int y = pagemap[id].gety();
+    int pagewidth = pagemap[id].get_width();
+    int pageheight = pagemap[id].get_height();
+    char cont = pagemap[id].get_content();
+    int idmin = 32768;
+    for (int h = y; h < y + pageheight; h++){
+        for (int w = x; w < x + pagewidth; w++){
+            int i = h * width + w;
+            if (boardlst[i].back() != cont){ //cont 바로 위의 페이지 탐색
+                int j = boardlst[i][find(boardlst[i].begin(), boardlst[i].end(), cont) - boardlst[i].begin() + 1];
+                if (j < idmin){ //후보 페이지 중 가장 index 작은 것 return
+                    idmin = j;
+                }
+            }
+        }
+    }
+    return idmin; // target page가 last page이면 idmin == 32768
 }
